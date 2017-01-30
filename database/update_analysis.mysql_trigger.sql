eef29b4edfc02ae2c3bb0d9afed9223c3f6111b2
chiefofsinners <alunlewis@gmail.com>
2017-01-30 18:37:09 +0000

    Corrected problem with previous commit

--- database/update_analysis.mysql_trigger.sql
+++ database/update_analysis.mysql_trigger.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TRIGGER `update_analysis` AFTER UPDATE ON `hosts_status`
  FOR EACH ROW BEGIN
 INSERT INTO hosts_analysis 
@@ -31,11 +30,6 @@ INSERT INTO hosts_analysis
   NEW.disk_timestamp,
   NEW.disk_usage,
   NEW.disk_timestamp
-=======
-CREATE TRIGGER `update_analysis` AFTER UPDATE ON `hosts_status` FOR EACH ROW BEGIN
-INSERT INTO hosts_analysis (host_name, cpu_max) 
- SELECT hosts_details.canonical_name, NEW.cpu_usage
->>>>>>> origin/master
   FROM hosts_details
    WHERE NEW.id = hosts_details.id
  ON DUPLICATE KEY
